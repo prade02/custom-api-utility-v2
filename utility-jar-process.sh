469f34ec81f51f436522513339c59f05a31c8ca2
Pradeep Kumar Manimuthu <prade.ycsm@gmail.com>
2021-08-25 12:03:08 +0530

    updated java command with dependency

--- utility-jar-process.sh
+++ utility-jar-process.sh
@@ -5,18 +5,21 @@ find ~/.gradle/caches/modules-2/files-2.1 "*.jar" -exec jar -xvf {} \;
 jar -cvf all.jar **/*.class
 
 # compile all java source files(this folder and sub folder) which has depencecies. create compiled files(classes) into the -d directory
-#javac -d bin/ **/*.java -cp /path/to/a/single/jar/file/:/path/to/a/single/jar/file
-#javac -d bin/ **/*.java -cp /path/to/a/single/consolidated/jar/file
+# javac -d bin/ **/*.java -cp /path/to/a/single/jar/file/:/path/to/a/single/jar/file
+# javac -d bin/ **/*.java -cp /path/to/a/single/consolidated/jar/file
 
 # run compiled java class - the class should have a public static void main(String[] args) method
-#java fully.qualified.name.ClassName
+# java fully.qualified.name.ClassName
+
+# run a java application with dependencies
+# java -cp .:/path/to/the/jar fully.qualified.class.name
 
 # creating jar - non runnable - just a java library. class files are from this and subdirectory
-#jar -cvf file-name.jar **/*.class
+# jar -cvf file-name.jar **/*.class
 
 # create a runnable jar
 # manifest.mf has Main-Class: fully.qualified.class.name.ClassName
-#jar -cvfm file-name.jar manifest.mf **/*.class
+# jar -cvfm file-name.jar manifest.mf **/*.class
 
 # run a jar file - runnable JAR
-#java -jar jar-file-name.jar
+# java -jar jar-file-name.jar
